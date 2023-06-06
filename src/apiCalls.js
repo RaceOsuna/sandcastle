6e57e0cfcdca470647a10ba6a8269cfc46900701
Race Osuna <raceosuna@gmail.com>
2023-06-06 17:11:24 -0600

    Refactor: re-organized api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,41 +1,40 @@
-//let customer, customerData, roomData, bookingData
+let customer, customerData, roomData, bookingData
 
 const customersResponse = fetch('http://localhost:3001/api/v1/customers')
 .then(response => response.json())
-//.then(response => console.log(response))
+
 
 const bookingsResponse = fetch('http://localhost:3001/api/v1/bookings')
 .then(response => response.json())
-//.then(response => console.log(response))
+
 
 const roomsResponse = fetch('http://localhost:3001/api/v1/rooms')
 .then(response => response.json())
-//.then(response => console.log(response))
 
-// window.addEventListener('load', () => {
-//   Promise.all([customersResponse, bookingsResponse, roomsResponse]).then(([customers, bookings, rooms]) => {
+
+
+  const promiseAll = () => Promise.all([customersResponse, bookingsResponse, roomsResponse]).then(([customers, bookings, rooms]) => {
   
-//   customerData = customers.customers
-//   bookingData = bookings.bookings
-//   roomData = rooms.rooms
+  customerData = customers.customers
+  bookingData = bookings.bookings
+  roomData = rooms.rooms
   
-//   const selectRandomUser = (data) => {
-//     const index = Math.floor(Math.random() * data.length - 1);
-//     const aUser = data[index];
-//     return aUser;
-//   }
+  const selectRandomUser = (data) => {
+    const index = Math.floor(Math.random() * data.length - 1);
+    const aUser = data[index];
+    return aUser;
+  }
 
-//   customer = selectRandomUser(customerData);
+  customer = selectRandomUser(customerData);
 
-//   return customer;
-//   })});
+  // return customer;
+  });
 
 
 export {
-  // customerData,
-  // bookingData,
-  // roomData
-  customersResponse,
-  bookingsResponse,
-  roomsResponse
+  promiseAll,
+  customer,
+  customerData,
+  bookingData,
+  roomData
 }
--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,4 @@
-import { customersResponse, bookingsResponse, roomsResponse /*customerData, roomData, bookingData*/ } from './apiCalls';
+import { /*customersResponse, bookingsResponse, roomsResponse*/ customer, bookingData, customerData, promiseAll} from './apiCalls';
 
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
@@ -8,26 +8,9 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 //import './images/turing-logo.png'
-let customer, customerData, roomData, bookingData
 
 window.addEventListener('load', () => {
-  Promise.all([customersResponse, bookingsResponse, roomsResponse]).then(([customers, bookings, rooms]) => {
-  
-  customerData = customers.customers
-  bookingData = bookings.bookings
-  roomData = rooms.rooms
-  
-  const selectRandomUser = (data) => {
-    const index = Math.floor(Math.random() * data.length - 1);
-    const aUser = data[index];
-    return aUser;
-  }
+  promiseAll().then(data => console.log(customer))
 
-  customer = selectRandomUser(customerData);
-
-  console.log(customer)
-  console.log(customerData)
-  console.log(bookingData)
-  console.log(roomData)
-  })});
+  });
 
