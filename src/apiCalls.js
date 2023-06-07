db96c17193c2fc2625eda76088468c572fe3cffd
Race Osuna <119973475+RaceOsuna@users.noreply.github.com>
2023-06-07 12:45:50 -0600

    Revert "Feat/tdd mock data"

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,40 +1,40 @@
-// let customer, customerData, roomData, bookingData
+let customer, customerData, roomData, bookingData
 
-// const customersResponse = fetch('http://localhost:3001/api/v1/customers')
-// .then(response => response.json())
-
-
-// const bookingsResponse = fetch('http://localhost:3001/api/v1/bookings')
-// .then(response => response.json())
+const customersResponse = fetch('http://localhost:3001/api/v1/customers')
+.then(response => response.json())
 
 
-// const roomsResponse = fetch('http://localhost:3001/api/v1/rooms')
-// .then(response => response.json())
+const bookingsResponse = fetch('http://localhost:3001/api/v1/bookings')
+.then(response => response.json())
+
+
+const roomsResponse = fetch('http://localhost:3001/api/v1/rooms')
+.then(response => response.json())
 
 
 
-//   const promiseAll = () => Promise.all([customersResponse, bookingsResponse, roomsResponse]).then(([customers, bookings, rooms]) => {
+  const promiseAll = () => Promise.all([customersResponse, bookingsResponse, roomsResponse]).then(([customers, bookings, rooms]) => {
   
-  // customerData = customers.customers
-  // bookingData = bookings.bookings
-  // roomData = rooms.rooms
+  customerData = customers.customers
+  bookingData = bookings.bookings
+  roomData = rooms.rooms
   
-  // const selectRandomUser = (data) => {
-  //   const index = Math.floor(Math.random() * data.length - 1);
-  //   const aUser = data[index];
-  //   return aUser;
-  // }
+  const selectRandomUser = (data) => {
+    const index = Math.floor(Math.random() * data.length - 1);
+    const aUser = data[index];
+    return aUser;
+  }
 
-  // customer = selectRandomUser(customerData);
+  customer = selectRandomUser(customerData);
 
-//   return customer;
-//   });
+  return customer;
+  });
 
 
-// export {
-//   promiseAll,
-//   customer,
-//   customerData,
-//   bookingData,
-//   roomData
-// }
+export {
+  promiseAll,
+  customer,
+  customerData,
+  bookingData,
+  roomData
+}
